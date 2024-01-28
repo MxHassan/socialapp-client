5d3609ac3b0025d4963bf81c41a2efa282304184
Mohamed Hassan <mxhassan195@gmail.com>
2024-01-28 23:12:43 +0200

    testing hash

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -15,27 +15,77 @@ import Feed from "../components/feed/Feed";
 import ProfileDetails from "../components/profiledetails/ProfileDetails";
 import PrivateRoutes from "../utils/PrivateRoutes";
 import PublicRoutes from "../utils/PublicRoutes";
-
-const router = createHashRouter(
-  createRoutesFromElements(
-    <Route errorElement={<ErrorPage />}>
-      <Route element={<PublicRoutes />}>
-        <Route element={<SignLayout />}>
-          <Route index path="signup" element={<SignUp />} />
-          <Route path="signin" element={<SignIn />} />
-        </Route>
-      </Route>
-      <Route element={<PrivateRoutes />}>
-        <Route element={<Home />}>
-          <Route path="/" element={<Feed />} />
-        </Route>
-        <Route element={<Profile />}>
-          <Route path="/profile" element={<ProfileDetails />} />
-          <Route path="/profile/:username" element={<ProfileDetails />} />
-        </Route>
-      </Route>
-    </Route>
-  )
-);
+const routes = [
+  {
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        element: <PublicRoutes />,
+        children: [
+          {
+            element: <SignLayout />,
+            children: [
+              {
+                path: "/signup",
+                element: <SignUp />,
+              },
+              {
+                path: "/signin",
+                element: <SignIn />,
+              },
+            ],
+          },
+        ],
+      },
+      {
+        element: <PrivateRoutes />,
+        children: [
+          {
+            element: <Home />,
+            children: [
+              {
+                path: "/",
+                element: <Feed />,
+              },
+            ],
+          },
+          {
+            element: <Profile />,
+            children: [
+              {
+                path: "/profile",
+                element: <ProfileDetails />,
+              },
+              {
+                path: "/profile/:username",
+                element: <ProfileDetails />,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
+const router = createHashRouter(routes);
+// createRoutesFromElements(
+//   <Route errorElement={<ErrorPage />}>
+//     <Route element={<PublicRoutes />}>
+//       <Route element={<SignLayout />}>
+//         <Route index path="signup" element={<SignUp />} />
+//         <Route path="signin" element={<SignIn />} />
+//       </Route>
+//     </Route>
+//     <Route element={<PrivateRoutes />}>
+//       <Route element={<Home />}>
+//         <Route path="/" element={<Feed />} />
+//       </Route>
+//       <Route element={<Profile />}>
+//         <Route path="/profile" element={<ProfileDetails />} />
+//         <Route path="/profile/:username" element={<ProfileDetails />} />
+//       </Route>
+//     </Route>
+//   </Route>
+// )
 
 export default router;
